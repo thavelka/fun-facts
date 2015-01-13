1e3a78163a0ae7f56d28c6dd9d22139eabcb717c
thavelka <tim.havelka@gmail.com>
2015-01-13 13:34:14 -0600

    moved fact chooser to new class FactBook

---
+++ app/src/main/java/com/thavelka/funfacts/FactBook.java
@@ -0,0 +1,34 @@
+package com.thavelka.funfacts;
+
+import java.util.Random;
+
+/**
+ * Created by tim on 1/13/15.
+ */
+public class FactBook {
+    // Member Variables
+
+    // Methods
+    public static String getFact() {
+        // Create facts array
+        String[] facts = {
+                "Ants stretch when they wake up in the morning.",
+                "Ostriches can run faster than horses.",
+                "Olympic gold medals are actually made mostly of silver.",
+                "You are born with 300 bones; by the time you are an adult you will have 206.",
+                "It takes about 8 minutes for light from the Sun to reach Earth.",
+                "Some bamboo plants can grow almost a meter in just one day.",
+                "The state of Florida is bigger than England.",
+                "Some penguins can leap 2-3 meters out of the water.",
+                "On average, it takes 66 days to form a new habit.",
+                "Mammoths still walked the earth when the Great Pyramid was being built." };
+
+        // Update fact label after button press
+        String fact;
+        // Randomly select a fact
+        Random randomGenerator = new Random();
+        int randomNumber = randomGenerator.nextInt(facts.length);
+        fact = facts[randomNumber];
+        return fact;
+    }
+}
--- app/src/main/java/com/thavelka/funfacts/FunFactsActivity.java
+++ app/src/main/java/com/thavelka/funfacts/FunFactsActivity.java
@@ -15,6 +15,7 @@ public class FunFactsActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fun_facts);
 
@@ -26,55 +27,13 @@ public class FunFactsActivity extends ActionBarActivity {
             @Override
             public void onClick(View v) {
 
-                // Update fact label after button press
-                String fact;
-                // Randomly select a fact
-                Random randomGenerator = new Random();
-                int randomNumber = randomGenerator.nextInt(3);
-
-                if (randomNumber == 0) {
-                    fact = "Ants stretch when they wake up in the morning.";
-                }
-                else if (randomNumber == 1) {
-                    fact = "Ostriches can run faster than horses.";
-                }
-                else if (randomNumber == 2) {
-                    fact = "Olympic gold medals are mostly made of silver.";
-                }
-                else {
-                    fact = "Error :(";
-                }
-
-                // Update label to dynamic fact
-                factLabel.setText(fact);
+            // Update label to dynamic fact
+            factLabel.setText(FactBook.getFact());
             }
         };
 
         showFactButton.setOnClickListener(listener);
 
-
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_fun_facts, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
